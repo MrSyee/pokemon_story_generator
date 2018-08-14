ef8dca47a628347990f29ba42047628a7220d2b2
Hy000jin <36581186+Hy000jin@users.noreply.github.com>
2018-08-14 19:34:25 +0900

    Hjkim - crawling different url (#12)
    * crawling different url
    
    * add crawler ipyb

--- 0_web_crawler/poke_wiki_crawl.py
+++ 0_web_crawler/poke_wiki_crawl.py
@@ -22,6 +22,12 @@ def get_desc(rounded_all):
         rounded = rounded_all[4]('td', class_="rounded")
         if rounded == []:
             rounded = rounded_all[6]('td', class_="rounded")
+            if rounded == []:
+                rounded = rounded_all[8]('td', class_="rounded")
+                if rounded == []:
+                    rounded = rounded_all[10]('td', class_="rounded")
+                    if rounded == []:
+                        rounded = rounded_all[12]('td', class_="rounded")
 
     desc = ""
     prev_d = ""
@@ -66,71 +72,78 @@ def get_egggroup(rounded_all):
     return egg_group
 
 # 1. url 불러오기
-url = "http://ko.pokemon.wikia.com/wiki/%EC%A0%84%EA%B5%AD%EB%8F%84%EA%B0%90/1%EC%84%B8%EB%8C%80"
-c = get_html(url)
+url_idx =1 # 전국도감 1세대 ~ 7세대까지
+url_number = 0 # file 저장용
+while url_idx < 8:
+    
+    print("Get different url : {}".format(url_idx))
+    url = "http://ko.pokemon.wikia.com/wiki/%EC%A0%84%EA%B5%AD%EB%8F%84%EA%B0%90/{}%EC%84%B8%EB%8C%80".format(url_idx)
+    url_number = url_idx
+    
+    c = get_html(url)
 
-# 2. url 내의 세부 링크 받아오기
-soup = BeautifulSoup(c, "html5lib")
-
-pages = []
-table = soup.find_all(class_="bg-white")
-
-pk_names = []
-prev_name = ""
-for i in range(len(table)):
-    pk_name = table[i]('td')[3].a.get('title')
-#     pk_name = table[i]('td')[3].a['title']
-    if prev_name == pk_name:
-        continue
-    prev_name = pk_name
-    link = table[i]('td')[3].a.get('href')
-#     link = table[i]('td')[3].a['href']
-    link = "http://ko.pokemon.wikia.com" + link
-    pk_name = pk_name.split(' ')[0]
-    pk_names.append(pk_name)
-    pages.append(link)
-
-# 3. url 내의 필요한 부분 크롤링
-i = 0
-pk_desc = []
-pk_types = []
-egg_groups = []
-print("Crawling Proceeding..")
-for page in pages:
-    print("[{}] {}".format(i, pk_names[i]))
-    # page = pages[2]
-    c = get_html(page)
+    # 2. url 내의 세부 링크 받아오기
     soup = BeautifulSoup(c, "html5lib")
 
-    rounded_all = soup.find_all("div", class_="rounded")
+    pages = []
+    table = soup.find_all(class_="bg-white")
 
-    # 도감 설명
-    desc = get_desc(rounded_all)
-    pk_desc.append(desc)
+    pk_names = []
+    prev_name = ""
+    for i in range(len(table)):
+        pk_name = table[i]('td')[3].a.get('title')
+    #     pk_name = table[i]('td')[3].a['title']
+        if prev_name == pk_name:
+            continue
+        prev_name = pk_name
+        link = table[i]('td')[3].a.get('href')
+    #     link = table[i]('td')[3].a['href']
+        link = "http://ko.pokemon.wikia.com" + link
+        pk_name = pk_name.split(' ')[0]
+        pk_names.append(pk_name)
+        pages.append(link)
 
-    # 속성
-    pk_type = get_type(rounded_all)
-    pk_types.append(pk_type)
+    # 3. url 내의 필요한 부분 크롤링
+    i = 0
+    pk_desc = []
+    pk_types = []
+    egg_groups = []
+    print("Crawling Proceeding..")
+    for page in pages:
+        print("[{}] {}".format(i, pk_names[i]))
+        # page = pages[2]
+        c = get_html(page)
+        soup = BeautifulSoup(c, "html5lib")
 
-    # 알 그룹
-    egg_group = get_egggroup(rounded_all)
-    egg_groups.append(egg_group)
+        rounded_all = soup.find_all("div", class_="rounded")
 
-    i += 1
+        # 도감 설명
+        desc = get_desc(rounded_all)
+        pk_desc.append(desc)
 
-# 4. data csv로 저장
-DATA_PATH = "./data/"
+        # 속성
+        pk_type = get_type(rounded_all)
+        pk_types.append(pk_type)
 
-pk_data = pd.DataFrame()
-pk_data['name'] = pk_names
-pk_data['desc'] = pk_desc
-pk_data['type'] = pk_types
-pk_data['egg_group'] = egg_groups
+        # 알 그룹
+        egg_group = get_egggroup(rounded_all)
+        egg_groups.append(egg_group)
 
-if not os.path.isdir(DATA_PATH):
-    os.mkdir(DATA_PATH)
+        i += 1
 
-# pk_data.to_csv(DATA_PATH + "pk_data_g1.csv")
-pk_data.to_csv(DATA_PATH + "pk_data_g1.csv", index=False, quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+    # 4. data csv로 저장
+    DATA_PATH = "./data/"
 
+    pk_data = pd.DataFrame()
+    pk_data['name'] = pk_names
+    pk_data['desc'] = pk_desc
+    pk_data['type'] = pk_types
+    pk_data['egg_group'] = egg_groups
 
+    if not os.path.isdir(DATA_PATH):
+        os.mkdir(DATA_PATH)
+
+    # pk_data.to_csv(DATA_PATH + "pk_data_g1.csv")
+    pk_data.to_csv(DATA_PATH + "pk_data_g{}.csv".format(url_number), index=False, quotechar='"', encoding='utf-8-sig', quoting=csv.QUOTE_NONNUMERIC)
+
+    url_idx += 1 # 전국도감 url 변경
